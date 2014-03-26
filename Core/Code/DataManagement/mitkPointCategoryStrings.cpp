44fd9500b7bb2b59f632550501bfff5bfe437802
Holger Roth <holger.roth@nih.gov>
2014-03-26 13:09:12 -0400

    feat-0001: remove enum of pt categories because unessesary.

--- Core/Code/DataManagement/mitkPointCategoryStrings.cpp
+++ Core/Code/DataManagement/mitkPointCategoryStrings.cpp
@@ -26,36 +26,36 @@ mitk::mitkPointCategoryStrings::mitkPointCategoryStrings()
 	m_PointSpecificationTypeStrings.push_back(std::string("True lymp node"));
 	m_PointSpecificationTypeStrings.push_back(std::string("False lymp node"));
 	//size
-	m_PointSpecificationTypeStrings.push_back(std::string("Size - short axis < 1cm"));
-	m_PointSpecificationTypeStrings.push_back(std::string("Size - short axis >=1cm"));
+	m_PointSpecificationTypeStrings.push_back(std::string("Size: short axis < 1cm"));
+	m_PointSpecificationTypeStrings.push_back(std::string("Size: short axis >=1cm"));
 	//density
-	m_PointSpecificationTypeStrings.push_back(std::string("Density - < 25HU"));
-	m_PointSpecificationTypeStrings.push_back(std::string("Density - >=25HU"));
+	m_PointSpecificationTypeStrings.push_back(std::string("Density: < 25HU"));
+	m_PointSpecificationTypeStrings.push_back(std::string("Density: >=25HU"));
 	//Morphology
-	m_PointSpecificationTypeStrings.push_back(std::string("Morphology - normal"));
-	m_PointSpecificationTypeStrings.push_back(std::string("Morphology - dysmorphic"));
-	m_PointSpecificationTypeStrings.push_back(std::string("Morphology - indeterminate"));
+	m_PointSpecificationTypeStrings.push_back(std::string("Morphology: normal"));
+	m_PointSpecificationTypeStrings.push_back(std::string("Morphology: dysmorphic"));
+	m_PointSpecificationTypeStrings.push_back(std::string("Morphology: indeterminate"));
 	//border
-	m_PointSpecificationTypeStrings.push_back(std::string("Border - smooth and distinct"));
-	m_PointSpecificationTypeStrings.push_back(std::string("Border - irregular and/or indistinct"));
-	m_PointSpecificationTypeStrings.push_back(std::string("Border - indeterminate"));
+	m_PointSpecificationTypeStrings.push_back(std::string("Border: smooth and distinct"));
+	m_PointSpecificationTypeStrings.push_back(std::string("Border: irregular and/or indistinct"));
+	m_PointSpecificationTypeStrings.push_back(std::string("Border: indeterminate"));
 	//hilum
-	m_PointSpecificationTypeStrings.push_back(std::string("Hilum - present"));
-	m_PointSpecificationTypeStrings.push_back(std::string("Hilum - not present"));
-	m_PointSpecificationTypeStrings.push_back(std::string("Hilum - indeterminate"));
+	m_PointSpecificationTypeStrings.push_back(std::string("Hilum: present"));
+	m_PointSpecificationTypeStrings.push_back(std::string("Hilum: not present"));
+	m_PointSpecificationTypeStrings.push_back(std::string("Hilum: indeterminate"));
 	//calcification
-	m_PointSpecificationTypeStrings.push_back(std::string("Calcification - not present"));
-	m_PointSpecificationTypeStrings.push_back(std::string("Calcification - < 50%"));
-	m_PointSpecificationTypeStrings.push_back(std::string("Calcification - >=50%"));
-	m_PointSpecificationTypeStrings.push_back(std::string("Calcification - indeterminate"));
+	m_PointSpecificationTypeStrings.push_back(std::string("Calcification: not present"));
+	m_PointSpecificationTypeStrings.push_back(std::string("Calcification: < 50%"));
+	m_PointSpecificationTypeStrings.push_back(std::string("Calcification: >=50%"));
+	m_PointSpecificationTypeStrings.push_back(std::string("Calcification: indeterminate"));
 	//proximity to adjacent structures
-	m_PointSpecificationTypeStrings.push_back(std::string("Proximity to adjacent structures - distinct from other structures"));
-	m_PointSpecificationTypeStrings.push_back(std::string("Proximity to adjacent structures - conglomerated to other structures"));
-	m_PointSpecificationTypeStrings.push_back(std::string("Proximity to adjacent structures - indeterminate"));
+	m_PointSpecificationTypeStrings.push_back(std::string("Proximity to adjacent structures: distinct from other structures"));
+	m_PointSpecificationTypeStrings.push_back(std::string("Proximity to adjacent structures: conglomerated to other structures"));
+	m_PointSpecificationTypeStrings.push_back(std::string("Proximity to adjacent structures: indeterminate"));
 	//orientation in the coronal or sagittal plane
-	m_PointSpecificationTypeStrings.push_back(std::string("Orientation in the coronal or sagittal plane - long axis parallel to spine"));
-	m_PointSpecificationTypeStrings.push_back(std::string("Orientation in the coronal or sagittal plane - long axis perpendicular to the long axis of the spine"));
-	m_PointSpecificationTypeStrings.push_back(std::string("Orientation in the coronal or sagittal plane - indeterminate (lymph node is spherical or long axis is oriented almost exactly 45 degrees to the long axis of the spine)"));
+	m_PointSpecificationTypeStrings.push_back(std::string("Orientation in the coronal or sagittal plane: long axis parallel to spine"));
+	m_PointSpecificationTypeStrings.push_back(std::string("Orientation in the coronal or sagittal plane: long axis perpendicular to the long axis of the spine"));
+	m_PointSpecificationTypeStrings.push_back(std::string("Orientation in the coronal or sagittal plane: indeterminate (lymph node is spherical or long axis is oriented almost exactly 45 degrees to the long axis of the spine)"));
 }
 
 std::vector<std::string> mitk::mitkPointCategoryStrings::getPointCategoryStrings()
